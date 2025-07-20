026ab06e89d89bfbbae53669018a165b853bdca9
aaryanwadhawan7 <aaryanwadhawam339@gmail.com>
2025-07-20 18:23:36 +0530

    Added Zod validation to our http-backend route endpoints

--- my-turborepo/apps/http-backend/src/index.ts
+++ my-turborepo/apps/http-backend/src/index.ts
@@ -3,16 +3,28 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 const app = express ();
 import { middleware } from "./middleware";
-
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types"
 
 // signin, signup and create-room endpoint
-app.post ('/signin', (req, res) => {
-    // userId, password -> Validation by Zod
+app.post ('/signup', (req, res) => {
+    const data = CreateUserSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json ({
+            "message" : "Invalid inputs"
+        })
+        return;
+    }
 })
 
 
-app.post ('/signup', (req, res) => {
-
+app.post ('/signin', (req, res) => {
+    const data = SigninSchema.safeParse(req.body);
+    if (!data.success) {
+        res.json ({
+            "message" : "Invalid inputs"
+        })
+        return;
+    }
 
     const userId = 1;
     const token = jwt.sign({
@@ -28,6 +40,14 @@ app.post ('/signup', (req, res) => {
 // user will be able join or create a room if it authorized
 // req.userId = (decoded as jwt.JWTPayload).userId as string
 app.post ('/room',middleware , (req, res) => {
+   const data = CreateRoomSchema.safeParse(req.body);
+   if (!data.success) {
+    res.json ({
+        "message" : "Invalid inputs"
+    })
+    return;
+   }
+   
     // db call
 
     res.json ({
